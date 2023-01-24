e70973df3009fc3373e3f9109f3b71eb56a8cdc9
x397428 <gabrielnicolllau1@gmail.com>
2023-01-24 16:12:33 -0300

    Ex051--(61)-v2.0

--- Ex051.py
+++ Ex051.py
@@ -1,26 +1,22 @@
-"""Exercício Python 51: desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10
-primeiros termos dessa progressão."""
+"""Exercício Python 61: refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
+termos da progressão usando a estrutura while."""
 
 
 print('=-' * 12)
 print('\033[33mOS 10 PRIMEIROS TERMOS DE UMA PA\033[m')
 print('=-' * 12)
 
-# Precisamos saber quem é o seu primeiro termo e qual a sua razão.
+print('=-' * 17)
+print('\033[33mOS 10 PRIMEIROS TERMOS DE UMA PA\033[m')
+print('=-' * 17)
 
 termo = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
 
-# Com o primeiro_termo, a partir dele, somar a razão, vamos encontrar os termos sucessores.
-# Exemplo: termo = 4
-#          razão = 2
-#          a1 = 4
-#          a2 = 4+ 2 = 6
-#          a3 = 6 + 2 = 8
+cont = 1
 
-# Fórmula: an = a1 + (n – 1)r
-pa = termo + (10 - 1) * razão  # Fórmula aplicado em python.
-for controle in range(termo, pa + 1, razão):  # Laço de repetição e pa + 1 para fazer os 10 termos.
-    print('{}'.format(controle), end=' ⇾ ')
-print('FIM!')
-
+while cont <= 10:
+    print('\033[32m{}\033[m \033[34m--> \033[m'.format(termo), end='')
+    cont += 1
+    termo += razão
+print('\033[33mFIM!\033[m')
