ccdbc6e25b2741cfe4b0c25868a93bf615413a2d
x397428 <gabrielnicolllau1@gmail.com>
2023-01-24 16:02:24 -0300

    Ex35--(42)-v2

--- Ex035-v1.0.py
+++ Ex035-v1.0.py
@@ -1,15 +1,29 @@
-# Exercício Python 035: desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou
-# não formar um triângulo.
+# Exercício Python 42: refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que categoria de triângulo
+# será formado:
+#
+# — EQUILÁTERO: todos os lados iguais.
+#
+# – ISÓSCELES: dois lados iguais, um diferente.
+#
+# – ESCALENO: todos os lados diferentes.
 
-# leia o comprimento de três retas
 reta1 = float(input('Primeiro segmento: '))
 reta2 = float(input('Segundo segmento: '))
 reta3 = float(input('Terceiro segmento: '))
 
-# "se a soma das medidas de dois deles é sempre maior que a medida do terceiro, então, eles podem formar um triângulo"
+# Verificar se vai formar um triângulo.
+# Se a soma das medidas de dois deles é sempre maior que a medida do terceiro, então, eles podem formar um triângulo.
+if reta3 + reta2 > reta1 and reta3 + reta1 > reta2 and reta1 + reta2 > reta3:
+    print('\033[32mÉ POSSÍVEL formar um TRIÂNGULO: \033[M', end='')
 
-if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
-    print('\033[32mÉ POSSÍVEL\033[m formar UM TRIÂNGULO com essas medidas.')
+    if reta1 == reta2 == reta3:  # EQUILÁTERO: todos os lados iguais.
+        print('EQUILÁTERO!')
+
+    elif reta1 != reta2 != reta3 != reta1:  # ISÓSCELES: dois lados iguais, um diferente.
+        print('ESCALENO!')
+
+    else:  # ISÓSCELES: dois lados iguais, um diferente.
+        print('ISÓSCELES!')
 
 else:
-    print('\033[31mNÃO É POSSÍVEL\033[m formar UM TRIÂNGULO com essas medidas.')
+    print('\033[31mNão é possível formar um TRIÂNGULO!\033[m')
